6f7b4bbc3a9971b8df512cc9b5aca768b36a3f2a
Till Theis <till_theis2@yahoo.de>
2012-04-24 20:06:51 +0200

    add files to use with janus

---
+++ .janus/update_bundles.sh
@@ -0,0 +1,35 @@
+
+# thanks to https://github.com/patrickdet (https://github.com/patrickdet/dotfiles/blob/master/bin/vimbundles.sh)
+mkdir -p ~/.janus
+cd ~/.janus
+
+get_bundle() {
+  (
+  if [ -d "$2" ]; then
+    echo "Updating $1's $2"
+    cd "$2"
+    git pull --rebase
+  else
+    git clone "https://github.com/$1/$2"
+  fi
+  )
+}
+
+get_bundle  Raimondi      delimitMate
+get_bundle  austintaylor  vim-indentobject
+get_bundle  clones        vim-align
+get_bundle  hallettj      jslint.vim
+get_bundle  kana          vim-textobj-user
+get_bundle  lukerandall   haskellmode-vim
+get_bundle  msanders      cocoa.vim
+#get_bundle  msanders      snipmate.vim
+get_bundle  nelstrom      vim-textobj-rubyblock
+#get_bundle  scrooloose    nerdtree
+get_bundle  tpope         vim-repeat
+get_bundle  tpope         vim-surround
+get_bundle  ujihisa       camelcasemotion
+get_bundle  vim-scripts   a.vim
+#get_bundle  altercation   vim-colors-solarized
+get_bundle  derekwyatt    vim-scala
+get_bundle  tpope         vim-rails
+#get_bundle  kien          ctrlp.vim
